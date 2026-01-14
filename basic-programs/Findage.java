9346ef124cf2c825fbd9afb5fc63209984a7796a
Devagrawal-tech <adev82990@gmail.com>
2026-01-14 22:43:39 +0530

    [Dev] Add . current age of Harry

---
+++ basic-programs/Findage.java
@@ -0,0 +1,10 @@
+public class Findage{
+	public static int currentage(int age){
+		return age - 2000;
+	}
+	public static void main(String[] args){
+		int age = 2024;
+		System.out.println("Harry age is " + currentage(age));
+	}
+	
+}
