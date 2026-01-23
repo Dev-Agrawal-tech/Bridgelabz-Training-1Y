236fc150b379ec2e4e3eefc6b016dddabff06135
Devagrawal-tech <adev82990@gmail.com>
2026-01-23 20:49:53 +0530

    [Dev] Add. Check Sum Of Natural number from for loop and formula is same ?

---
+++ basic-programs/SumOfNumberNatural.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class SumOfNumberNatural{
+
+	public static int SumOfNaturalNumberUsingFormula(int num){
+		int total_formula= num*(num+1)/2;
+		return total_formula;
+	}
+	public static int SumOfNaturalNumberUsingLoop(int num){
+		int total=0;
+		for(int i=num ; i>0 ; i--){
+			total=total + i;
+		}
+		
+		return total;
+		
+		//int total_formula=SumOfNaturalNumberUsingFormula(num);
+	}
+	public static boolean isNatural(int num){
+		if(num<0){
+			
+			//System.out.println("It's not a natural number");
+			
+			return false;
+		}
+		else{
+			
+			//SumOfNaturalNumberUsingLoop(num);
+			
+			return true;
+		}
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value ");
+		int num = sc.nextInt();
+		
+		//isNatural(num);
+		
+		if(isNatural(num)){
+			int total_From_loop=SumOfNaturalNumberUsingLoop(num);
+			int total_From_Formula=SumOfNaturalNumberUsingFormula(num);
+			
+			if(total_From_loop == total_From_Formula){
+				System.out.println(total_From_Formula);
+			}
+			
+		}
+		else{
+			System.out.println("It's not a natural number");
+		}
+	}
+}
