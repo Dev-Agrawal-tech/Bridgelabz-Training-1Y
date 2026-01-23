7df78108003b9535940771b2b9e8a218ad5dc19e
Devagrawal-tech <adev82990@gmail.com>
2026-01-23 20:50:31 +0530

    [Dev] Add. Find the Factoiral using while loop

---
+++ basic-programs/Factorial.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+public class Factorial{
+	public static boolean positiveNumber(int num){
+		if(num<0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value ");
+		int num=sc.nextInt();
+		int fact=1;
+		int number=num;
+		if(positiveNumber(num)){
+			while(num>0){
+				fact = fact*num;
+				num--;
+			}
+		}
+		else{
+			System.out.println("You entered wrong number , ");
+			System.out.println("Please! Enter Positive number . ");
+		}
+		System.out.println("Factorial of " + number + " is " + fact);
+	}
+}
